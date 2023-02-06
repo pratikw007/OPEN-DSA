d3bb6653d29a33169e611d77b2e81b3d48a02b8a
Emma Paris <eparis@atlassian.com>
2023-02-06 21:58:47 +0530

    Commit

--- InverteStar.java
+++ InverteStar.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class InverteStar {
     public static void main(String[] args) {
-        int n = 4;
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        
+       while(t--> 0){
+
+        int n = sc.nextInt();
+    
         for (int line = 1; line <= n; line++) {
             for (int star = 1; star <= n - line + 1; star++) {
                 System.out.print("*");
             }
             System.out.println();
-        }
+        }   
+      
+    }
     }
 }
---
+++ Practice.java
@@ -0,0 +1,18 @@
+public class Practice {
+
+
+
+    static void Pratice1(){
+        String[] cars = {"Volvo","BMW","Mercedize","Ford"};
+        int[] myNum = {10,20,30};
+        cars[0] = "Opel";
+        System.out.println(cars[0]);
+
+
+
+    }
+    public static void main(String[] args) {
+          Pratice1();
+    }
+    
+}
